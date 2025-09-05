60b71fcd4e894363a41e30ee1f66125419ca51d2
tunguyenn99 <tunguyen150599@gmail.com>
2025-09-05 22:06:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Bước 1 
+uv init crawl-topcv-jobs
+
+Bước 2
+uv add pandas matplotlib seaborn ipykernel
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from crawl-topcv-jobs!")
+
+
+if __name__ == "__main__":
+    main()
